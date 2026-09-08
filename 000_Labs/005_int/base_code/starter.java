9887d2b957712d1c34952cd389fa83f38944edcf
ashleyeheo <ashley.stella0101@gmail.com>
2026-09-08 20:36:21 +0000

    update

--- 000_Labs/005_int/base_code/starter.java
+++ 000_Labs/005_int/base_code/starter.java
@@ -9,6 +9,8 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println(1); 
+		System.out.println(-25);
+		System.out.print(-2);
 	}
 }
--- 000_Labs/006_doubles/base_code/starter.java
+++ 000_Labs/006_doubles/base_code/starter.java
@@ -9,6 +9,8 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.print("The Celsius value for "+32.0+" degrees Fahrenheit is "+0.0+" degrees."); 
 	}
 }
+
+	
