411fa7d2e5e5cf9eb940565a2d1a0bb183dddc77
ashleyeheo <ashley.stella0101@gmail.com>
2026-09-17 20:39:27 +0000

    update

--- 102_Exercises/09_Restaurant/basecode/starter.java
+++ 102_Exercises/09_Restaurant/basecode/starter.java
@@ -7,7 +7,29 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
-		// Your code goes below here
+Scanner sc = new Scanner(System.in);
+
+
+		System.out.println("=================================================");
+		System.out.println("            WELCOME RESTAURANT OWNER!            ");
+		System.out.println("=================================================");
+		System.out.println();
+		String questionName = "What is your name?"; 	
+		System.out.println(questionName);	
+		String replyName = sc.nextLine();
+		System.out.println();
+		String questionRestaurant = "What is the name of your restaurant?";
+		System.out.println(questionRestaurant);
+		String replyRestaurant = sc.nextLine();
+		System.out.println();
+		System.out.println("Great to see you, " + replyName + "! Let's set up a menu for " + replyRestaurant + "!");
+		System.out.println("Tonight's menu has room for exactly three iems. Let's go!");
+		System.out.println();
+		System.out.println("--- Item #1 ---");
+		String replyItem1 = sc.nextLine();
+		System.out.println("Item name: " + replyItem1);
 		
+
+	
 	}
 }
