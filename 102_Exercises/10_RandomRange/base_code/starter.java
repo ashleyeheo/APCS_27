814bb386f23f5c4df4eb2d1390eac44710a8a647
ashleyeheo <ashley.stella0101@gmail.com>
2026-09-22 20:35:25 +0000

    update

--- 102_Exercises/10_RandomRange/base_code/starter.java
+++ 102_Exercises/10_RandomRange/base_code/starter.java
@@ -9,6 +9,27 @@ import java.util.Scanner;
 class starter {
 	public static void main(String args[]) {
 
+Scanner sc = new Scanner(System.in);
+Math.method(); 
+
+System.out.println("Enter two numbers to create a range for your random number");
+System.out.println("Please enter an integer: ");
+int numberone = sc.nextInt();
+System.out.println("Please enter another integer (bigger than the first): ");
+int numbertwo = sc.nextInt();
+System.out.println();
+System.out.println(numberone + " - " + numbertwo);
+System.out.println("Here are five numbers generated in that range.");
+int numberthree = numbertwo - numberone;
+double a = Math.random()*numberthree;
+double b = Math.random()*numberthree;
+double c = Math.random()*numberthree;
+double d = Math.random()*numberthree;
+double e = Math.random()*numberthree;
+System.out.print(a + b + c + d + e);
+
+
+
 
 
 
