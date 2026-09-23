4463870ea31639b890beec3781f6675b30f3c34b
ashleyeheo <ashley.stella0101@gmail.com>
2026-09-23 20:51:41 +0000

    update

--- 102_Exercises/12_FortuneCookie/base_code/starter.java
+++ 102_Exercises/12_FortuneCookie/base_code/starter.java
@@ -8,6 +8,49 @@ import java.util.Scanner;
 
 class starter {
 	public static void main(String args[]) {
+
+Scanner sc = new Scanner(System.in);
+System.out.println("big money fortune");
+System.out.print("pay for a fortune: ");
+int Money = (sc.nextInt());
+
+int smallMoney = 1;
+int mediumMoney = 10;
+int bigMoney = 100;
+int bigbigMoney = 1000;
+int iluvashley = 10000;
+int bigbigbigMoney = 100000;
+int hugeMoney = 1000000;
+int gigaMoney = 100000000;
+
+
+
+
+if(Money == 0.1){
+System.out.print(" : you sit up and find that you are wrapped in burnous on a bed of dry heather soft and sweet smelling");
+}
+
+if(Money == 1){
+System.out.print("you make paper, pens, and ink");
+}
+
+if(Money == 10){
+System.out.print("the jailer is about to appear");	
+}
+
+if(Money == 100){
+System.out.print("");	
+}
+
+	
+
+
+
+
+
+
+
+
 		
 	}
 }
